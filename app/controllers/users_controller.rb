e5ab08f96609b1a2f766c9721284d15193ebeb0c
Defoe.Brand <defoe.brand@gmail.com>
2020-09-17 16:44:50 +0300

    Add form to create friendship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @friendships = Friendship.all
+    @friendship = Friendship.new
+    # @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+  end
+
+  def friend_requests
+    @friendship = Friendship.new(friend_params)
+
+    @friendship.save
+  end
+
+  private
+
+  def friend_params
+    params.require(:friendship).permit(:friend_id, :user_id)
   end
 end
