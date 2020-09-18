0b916d4d27038eb44dce20187a8f50f155c20b31
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-09-18 23:22:03 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,6 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
 
-  post '/friend_request', :to => 'users#friend_requests'
-  patch '/accept_request', :to => 'users#accept_request'
-
-  post 'new_request', :to => 'friendships#create'
-
-  put '/accept_request', :to => 'users#accept_request'
-  put '/deny_request', :to => 'friendships#deny'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
