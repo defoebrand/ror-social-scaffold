d53aaf6ffba42f9cf558710adec54cb9a865387d
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-09-21 13:05:33 +0800

    moved show logic to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,7 @@
 module UsersHelper
-  def friendship_exists?(user)
-    
-  end
 
-  def friend_status(user)
-    @status = content_tag(:div)
+  def friend_status_index(user)
+    @status = tag(:div, class: 'status-btn-tag')
     unless @friends.any? {|f| f.friend_id == user.id or f.user_id == user.id}
       @status << button_to('Request', {:controller => 'friendships', 
         :action => 'create', 
@@ -35,10 +32,52 @@ module UsersHelper
         end
       end
     end
-    @status.html_safe
-
-
+    @status
   end
+
+
+  def friend_request_show(user)
+    friendship_status = Friendship.find_by(friend_id: user.id)
+    @status = tag(:div)
+    unless friendship_status
+      @status << button_to('Request', {:controller => 'friendships', 
+        :action => 'create', 
+        :user_id => current_user.id, 
+        :friend_id => user.id,
+        }, :method => :post)
+      return @status.html_safe
+    end
+  end
+
+  def friend_status_show(user)
+    friend = Friendship.find_by(friend_id: current_user.id)
+    @content = tag(:div)
+    if friend
+      @content << content_tag(:p, "#{friend.user.name} requested to be your friend")
+    end
+    if friend and friend.status == 'pending'
+      @content << button_to('Accept', {:controller => 'friendships', 
+        :action => 'update', 
+        :id => friend.id,   
+        :status => 'confirmed'
+        }, :method => :put)
+
+       @content << button_to('Reject', {:controller => 'friendships', 
+        :action => 'update', 
+        :id => friend.id,   
+        :status => 'denied'
+        }, :method => :put)
+    elsif friend and (friend.status == 'denied' or friend.status == 'confirmed')
+      @content << content_tag(:p, friend.status)
+    else  
+      friend = Friendship.find_by(friend_id: @user.id, user_id: current_user.id)
+      if friend
+        @content << content_tag(:p, friend.status)
+      end
+    end
+    @content.html_safe
+  end
+
 
 end
 
