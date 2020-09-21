f175e04df321b1c57efd974cd505247f12562116
Defoe.Brand <defoe.brand@gmail.com>
2020-09-21 18:55:55 +0300

    Adjust code per rubocop

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,17 +2,12 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = Friendship.new(user_id: params[:user_id], friend_id: params[:friend_id], status: 'pending')
 
-    if @friendship.save!
-      redirect_to request.referrer
-    end
+    redirect_to request.referrer if @friendship.save!
   end
 
   def update
     @friend = Friendship.find(params[:id])
     @friend.status = params[:status]
-    if @friend.save!
-      redirect_to request.referrer
-    end
+    redirect_to request.referrer if @friend.save!
   end
-
 end
