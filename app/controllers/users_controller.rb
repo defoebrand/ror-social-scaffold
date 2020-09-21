0e33db146dfc4382c4e979ce90b6bde9b2b6202e
Defoe.Brand <defoe.brand@gmail.com>
2020-09-21 16:56:24 +0300

    Condense helper methods and clean up unnecessary code in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @others = @users.select {|f| f.id != current_user.id}
+    @others = @users.select { |f| f.id != current_user.id }
     @friendships = Friendship.all
     @friendship = Friendship.new
     @friends = @friendships.select { |x| (x.user_id == current_user.id) || (x.friend_id == current_user.id) }
-    @friend_requests = @friendships.select { |x| x.friend_id == current_user.id }
-    # @not_friends = @users.each do |x|
-    #   if x.id != current_user.id
-    #     if @friends.empty?
-    #       x.id
-    #     else
-    #       @friends.each do |y|
-    #         x.id unless x.id == y.friend_id
-    #       end
-    #     end
-    #   end
-    # end
-    # @user = current_user
   end
 
   def show
@@ -27,19 +14,7 @@ class UsersController < ApplicationController
     @posts = @user.posts.ordered_by_most_recent
     @friendship = Friendship.new
     @friendships = Friendship.all
-    @friends = @friendships.select { |x| x.user_id == current_user.id }
-  end
-
-  def friend_requests
-    @friendship = Friendship.create(friend_params)
-
-    redirect_to request.referrer
-  end
-
-  def accept_request
-    @friendship = Friendship.find(friend_params[:id])
-    @friendship.update_attributes(friend_params)
-    redirect_to request.referrer
+    @friends = @friendships.select { |x| (x.user_id == current_user.id) || (x.friend_id == current_user.id) }
   end
 
   private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,67 +1,45 @@
 module UsersHelper
   def friend_status_index(user)
     @status = tag(:div, class: 'status-btn-tag')
+    button_changer(@status, user)
+  end
+
+  def friend_response_show(user)
+    @content = tag(:div)
+    button_changer(@content, user)
+  end
+
+  private
+
+  def button_changer(content, user)
     if @friends.any? { |f| f.friend_id == user.id or f.user_id == user.id }
       friend = Friendship.find_by(friend_id: current_user.id, user_id: user.id)
       if friend and friend.status == 'pending'
-        @status << button_to('Accept', { controller: 'friendships',
+        content << content_tag(:p, "#{friend.user.name} requested to be your friend")
+        content << tag(:br)
+        content << button_to('Accept', { controller: 'friendships',
                                          action: 'update',
                                          id: friend.id,
                                          status: 'confirmed' }, method: :put)
 
-        @status << button_to('Reject', { controller: 'friendships',
+        content << button_to('Reject', { controller: 'friendships',
                                          action: 'update',
                                          id: friend.id,
                                          status: 'denied' }, method: :put)
-      elsif friend && (friend.status == 'denied' || friend.status == 'confirmed')
-        @status << content_tag(:p, "Request #{friend.status}", class: 'status')
+      elsif friend and (friend.status == 'denied' or friend.status == 'confirmed')
+        content << content_tag(:p, "Request #{friend.status}", class: 'status')
+
       else
         friend = Friendship.find_by(friend_id: user.id, user_id: current_user.id)
-        @status << content_tag(:p, "Request #{friend.status}", class: 'status') if friend
+        content << content_tag(:p, "Request #{friend.status}", class: 'status') if friend
       end
     else
-      @status << button_to('Request Friendship', { controller: 'friendships',
+      content << button_to('Request Friendship', { controller: 'friendships',
                                                    action: 'create',
                                                    user_id: current_user.id,
                                                    friend_id: user.id }, method: :post)
-      @status.html_safe
+      content.html_safe
     end
-    @status
+    content
   end
-
-  def friend_request_show(user)
-    friendship_status = Friendship.find_by(friend_id: user.id, user_id: current_user.id) || Friendship.find_by(user_id: user.id, friend_id: current_user.id)
-    @status = tag(:div)
-    unless friendship_status
-      @status << button_to('Request Friendship', { controller: 'friendships',
-                                                   action: 'create',
-                                                   user_id: current_user.id,
-                                                   friend_id: user.id }, method: :post)
-      @status.html_safe
-    end
-  end
-
-  def friend_status_show(user)
-    friend = Friendship.find_by(friend_id: user.id, user_id: current_user.id) || Friendship.find_by(user_id: user.id, friend_id: current_user.id)
-    @content = tag(:div)
-    if friend and friend.friend.name == current_user.name and friend.status == 'pending'
-      @content << content_tag(:p, "#{friend.user.name} requested to be your friend")
-
-      @content << button_to('Accept', { controller: 'friendships',
-                                        action: 'update',
-                                        id: friend.id,
-                                        status: 'confirmed' }, method: :put)
-
-      @content << button_to('Reject', { controller: 'friendships',
-                                        action: 'update',
-                                        id: friend.id,
-                                        status: 'denied' }, method: :put)
-    elsif friend and (friend.status == 'denied' or friend.status == 'confirmed')
-      @content << content_tag(:p, "Request #{friend.status}", class: 'status')
-    else
-      friend = Friendship.find_by(friend_id: @user.id, user_id: current_user.id)
-      @content << content_tag(:p, "Request #{friend.status}", class: 'status') if friend
-      end
-    @content.html_safe
-    end
 end
