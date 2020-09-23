8e622ebeb6c9b316b73a4fc74db397a296921eb2
Defoe.Brand <defoe.brand@gmail.com>
2020-09-23 12:55:10 +0300

    Adjust code per GitHub Actions

--- spec/features/timeline_spec.rb
+++ spec/features/timeline_spec.rb
@@ -5,7 +5,6 @@ RSpec.feature 'Timeline', type: :feature do
     before(:each) do
       User.create(name: 'user1', email: 'user@user.com',
                   password: 'passwords', password_confirmation: 'passwords')
-
       Post.create(user_id: 1, content: 'another post')
       visit new_user_session_path
       within('form') do
@@ -29,6 +28,31 @@ RSpec.feature 'Timeline', type: :feature do
         fill_in 'Content', with: 'A new post'
       end
       expect { click_button 'Save' }.to change(Post, :count).by(1)
+    end
+  end
+end
+
+RSpec.feature 'Timeline', type: :feature do
+  context 'Create new posts and comments' do
+    before(:each) do
+      User.create(name: 'user1', email: 'user@user.com',
+                  password: 'passwords', password_confirmation: 'passwords')
+      Post.create(user_id: 1, content: 'another post')
+      visit new_user_session_path
+      within('form') do
+        fill_in 'Email', with: 'user@user.com'
+        fill_in 'Password', with: 'passwords'
+      end
+      click_button 'Log in'
+      visit root_path
+      within('form.new_post') do
+        fill_in 'Content', with: 'A new post'
+      end
+      click_button 'Save'
+    end
+    it 'visits the root path after logging in' do
+      visit root_path
+      expect(page).to have_content('Recent posts')
     end
     it 'creates a new comment' do
       visit root_path
